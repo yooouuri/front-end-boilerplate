b5d4d7bb3706d51b007ef93be11eb0093de8c0c8
Tom <tom.van.veen@visma.com>
2024-09-19 16:28:58 +0200

    style: update PageSection component styles

--- app/components/PageSection.vue
+++ app/components/PageSection.vue
@@ -5,8 +5,8 @@ const props = defineProps<{
 </script>
 
 <template>
-  <section class="rounded shadow bg-slate-200 dark:bg-slate-800 border border-slate-800 dark:border-slate-200">
-    <header class="border-t border-b-slate-800 dark:border-b-slate-200 p-4 border-b">
+  <section class="rounded shadow shadow-slate-100 dark:shadow-slate-900 bg-slate-200 dark:bg-slate-800 border border-slate-500 dark:border-slate-600">
+    <header class="border-b border-b-slate-200 dark:border-b-slate-600 p-4">
       <h2 class="text-center text-2xl font-bold tracking-wide text-slate-700 dark:text-slate-300">
         {{ props.title }}
       </h2>
