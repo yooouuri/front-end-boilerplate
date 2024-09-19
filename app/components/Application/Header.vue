1e4abe4ef05cb2a288ab90cc60789791bdcbfe6e
Tom <tom.van.veen@visma.com>
2024-09-19 16:38:00 +0200

    task: add github button to header

--- app/components/Application/Header.vue
+++ app/components/Application/Header.vue
@@ -1,7 +1,12 @@
 <template>
   <header class="bg-zinc-100 border-b border-b-zinc-200 dark:bg-zinc-700 dark:border-b-zinc-600 py-8">
-    <UContainer>
-      Header
+    <UContainer class="flex items-center">
+      <span>
+        Header
+      </span>
+      <div class="ml-auto">
+        <UButton to="https://github.com/spend-cloud-tom/front-end-boilerplate" icon="mdi:github" color="blue" label="GitHub repository" variant="outline" />
+      </div>
     </UContainer>
   </header>
 </template>
